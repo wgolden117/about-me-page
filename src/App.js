61034bf76b22a4d17fb30d8201d67d72ded8e5cd
Weronika Golden <wgolden117@gmail.com>
2025-07-29 13:13:55 -0700

    Re-organized files and updated Bio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+// Import images
+import profilePhoto from './assets/images/20240122_103005.jpg';
+import photo1 from './assets/images/20230819_115821.jpg';
+import photo2 from './assets/images/20230817_160651.jpg';
+import photo3 from './assets/images/IMG_20220817_162247.jpg';
+import photo4 from './assets/images/IMG_20230129_193532.jpg';
+import photo5 from './assets/images/IMG_20230129_193537.jpg';
+import photo6 from './assets/images/SunDown.jpg';
+import photo7 from './assets/images/20240627_105335.jpg';
+import photo8 from './assets/images/GreenKnitSweater.jpg';
+import photo9 from './assets/images/BlueKnitSweater.jpg';
+
+// Import video thumbnails
+import thumb1 from './assets/images/video-thumbnail1.jpg';
+import thumb2 from './assets/images/video-thumbnail2.jpg';
+
+// Import videos
+import video1 from './assets/videos/steel.mp4';
+import video2 from './assets/videos/USPSA.mp4';
 
 function App() {
   // Create references to different sections of the page to allow scrolling to them
@@ -89,7 +108,6 @@ function App() {
     };
   }, []);
 
-
   return (
       <>
         {/* Stars across the entire page */}
@@ -105,54 +123,56 @@ function App() {
   <div className="App">
     <header className="App-header">
       <h1 className="kalnia-glaze-header">About Me</h1>
-          <img src={`${process.env.PUBLIC_URL}/20240122_103005.jpg`} alt="Weronika Golden" className="profile-photo"/>
+      <img src={profilePhoto} alt="Weronika Golden" className="profile-photo"/>
 
-          <p className="small-text bold-text profile-text">
-            Hi! My name is Weronika Golden. I’m a software engineering student at Arizona State University, with a
-            passion for developing software solutions and solving problems using technology.
+      <p className="small-text bold-text profile-text">
+        Hi! My name is Weronika Golden. I’m a software engineering student at Arizona State University, with a
+        passion for developing software solutions and solving problems using technology.
 
-            I have experience in Java, C, Python, and SQL. I’ve also worked on various database management systems,
-            and in my free time, I work on personal projects!
+        I have experience in Java, C, Python, and SQL. I’ve also worked on various database management systems,
+        and in my free time, I work on personal projects!
 
-            I love exploring new technologies and working on challenging projects that allow me to expand my skill
-            set.
-          </p>
+        I love exploring new technologies and working on challenging projects that allow me to expand my skill
+        set.
+      </p>
 
-          {/* Modal */}
+      {/* Modal */}
       <div id="imageModal" className="modal">
         <span className="close">&times;</span>
         <div className="modal-content-wrapper">
           <img className="modal-content" id="modalImage" alt=""/>
           <div id="caption"></div>
         </div>
-        </div>
+      </div>
 
-        {/* Links that trigger scrolling */}
-        <div>
-          <button onClick={() => scrollToSection(bioRef)} className="link-button">Bio</button>
-          <button onClick={() => scrollToSection(photosRef)} className="link-button">Photos</button>
-          <button onClick={() => scrollToSection(videosRef)} className="link-button">Videos</button>
+      {/* Links that trigger scrolling */}
+      <div>
+        <button onClick={() => scrollToSection(bioRef)} className="link-button">Bio</button>
+        <button onClick={() => scrollToSection(photosRef)} className="link-button">Photos</button>
+        <button onClick={() => scrollToSection(videosRef)} className="link-button">Videos</button>
 
-          {/* GitHub Link Button */}
-          <a href="https://github.com/wgolden117" target="_blank" rel="noopener noreferrer">
-            <button className="link-button">GitHub</button>
-          </a>
-        </div>
+        {/* GitHub Link Button */}
+        <a href="https://github.com/wgolden117" target="_blank" rel="noopener noreferrer">
+          <button className="link-button">GitHub</button>
+        </a>
+      </div>
 
       <div ref={bioRef} className="bio-section">
         <h1 className="section-title">Detailed Bio</h1>
         <p className="small-text bold-text">
-          I first attended Arizona State University after obtaining my Associates Degree from
-          my local Community College. Then, I attended ASU online and graduated Summa Cum Laude
-          with a Bachelors degree in Criminology and Criminal Justice.
+          I first attended Arizona State University (ASU) in the Fall of 2016 after obtaining my
+          Associates Degree from my local Community College, the College of Dupage. I obtained
+          a BS in Criminology and Criminal Justice, graduating Summa Cum Laude in 2018!
         </p>
         <p className="small-text bold-text">
-          I worked full-time while obtaining my Associates and BS Degree and for a period of time I even worked
-          multiple jobs while attending school. After graduating, I continued working in accounts payable and in
-          the following years, I also worked in human resources, customer service, and sales.
+          I worked full-time while obtaining my Associates and BS Degree, and for a period of time I even worked
+          multiple jobs while attending school. After graduating, I continued working in my current field, accounts
+          payable, and in
+          the following years, I also worked in human resources, customer service, reception, and sales.
         </p>
         <p className="small-text bold-text">
-          I'm originally from Chicago. I grew up there, and lived in the Suburbs until my husband and I moved
+          I'm originally from Chicago. I grew up there, and lived in the Suburbs (Carol Stream, IL) until my husband and
+          I moved
           to Ogden, Utah. We lived there for about 4 years, and we are now living in Post Falls, ID with our
           3 beloved cats.
         </p>
@@ -160,13 +180,14 @@ function App() {
           Throughout my working career, I discovered a passion for programming, and made the decision to go back to
           school. I am now working towards my second BS, this time in Software Engineering. I decided to work
           towards a second BS instead of a Masters because the BS is accredited by the Fulton School of Engineering
-          with ASU and my anticipated graduation date is May of 2026!
+          with ASU (yes, I went to ASU again!) and my anticipated graduation date is May of 2026!
         </p>
         <p className="small-text bold-text">
           My personal hobbies include competitive shooting as part of the United States Practical Shooting
-          Association, snowboarding, playing the piano and violin, knitting, and gaming. I also love the
+          Association (USPSA), snowboarding, playing the piano and violin, knitting, and gaming. I also love the
           outdoors, love to camp and hike, and I absolutely love trying and eating delicious foods
-          from various cultures! My favorite so far, other than my native Polish foods, is Japanese cuisine!
+          from various cultures! My favorite so far, other than my native Polish foods, is Japanese cuisine as well a
+          delicious rare steak!
           It's hard to choose a favorite hobby, I love spending my free time engaging in creative and fun tasks.
           When I'm tired or have had a long day, I unwind by relaxing and watching my favorite movies or shows
           and eating my favorite snacks!
@@ -179,47 +200,37 @@ function App() {
       </div>
 
       <section ref={photosRef} className="photo-gallery">
-          <h1 className="section-title">Photos</h1>
-          <div className="photo-grid">
-            <img src={`${process.env.PUBLIC_URL}/20230819_115821.jpg`} alt="Tetons"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/20230817_160651.jpg`} alt="Tetons-hiking"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/IMG_20220817_162247.jpg`} alt="Tetons-Horseback Riding"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/IMG_20230129_193532.jpg`} alt="Snowboarding"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/IMG_20230129_193537.jpg`} alt="Snowboarding-Powder Mountain"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/SunDown.jpg`} alt="Top of Sundown lift - Powder Mountain"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/20240627_105335.jpg`} alt="Our Three Cats!"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/GreenKnitSweater.jpg`} alt="Green Knit Sweater I made"
-                 className="clickable-image square-photo"/>
-            <img src={`${process.env.PUBLIC_URL}/BlueKnitSweater.jpg`} alt="Blue Purl Knit Crop-top Sweater I made"
-                 className="clickable-image square-photo"/>
-          </div>
-          <button className="return-button" onClick={scrollToTop}>Return to Top</button>
-        </section>
+        <h1 className="section-title">Photos</h1>
+        <div className="photo-grid">
+          <img src={photo1} alt="Tetons" className="clickable-image square-photo"/>
+          <img src={photo2} alt="Tetons-hiking" className="clickable-image square-photo"/>
+          <img src={photo3} alt="Tetons-Horseback Riding" className="clickable-image square-photo"/>
+          <img src={photo4} alt="Snowboarding" className="clickable-image square-photo"/>
+          <img src={photo5} alt="Snowboarding-Powder Mountain" className="clickable-image square-photo"/>
+          <img src={photo6} alt="Top of Sundown lift - Powder Mountain" className="clickable-image square-photo"/>
+          <img src={photo7} alt="Our Three Cats!" className="clickable-image square-photo"/>
+          <img src={photo8} alt="Green Knit Sweater I made" className="clickable-image square-photo"/>
+          <img src={photo9} alt="Blue Purl Knit Crop-top Sweater I made" className="clickable-image square-photo"/>
+        </div>
+        <button className="return-button" onClick={scrollToTop}>Return to Top</button>
+      </section>
 
       <section ref={videosRef} className="videos-section">
         <h1 className="section-title">Videos</h1>
-          <video width="400" height="400" controls preload="none"
-                 poster={`${process.env.PUBLIC_URL}/video-thumbnail1.jpg`}>
-            <source src={`${process.env.PUBLIC_URL}/steel.mp4`} type="video/mp4"/>
-            Your browser does not support the video tag.
-          </video>
-          <video width="400" height="400" controls preload="none"
-                 poster={`${process.env.PUBLIC_URL}/video-thumbnail2.jpg`}>
-            <source src={`${process.env.PUBLIC_URL}/USPSA.mp4`} type="video/mp4"/>
-            Your browser does not support the video tag.
-          </video>
-          <button className="return-button" onClick={scrollToTop}>Return to Top</button>
+        <video width="400" height="400" controls preload="none" poster={thumb1}>
+          <source src={video1} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+        <video width="400" height="400" controls preload="none" poster={thumb2}>
+          <source src={video2} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+        <button className="return-button" onClick={scrollToTop}>Return to Top</button>
       </section>
     </header>
   </div>
       </>
   );
 }
+
 export default App;
